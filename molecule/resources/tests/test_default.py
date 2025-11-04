bbe7eac42bce6739fdb47e99a101af4ee327f538
khaledk2 <khaledk2@hotmail.com>
2025-11-04 10:07:26 +0000

    fix

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -7,5 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_nginx_gateway(host):
-    out = host.check_output('curl -L localhost')
+    out = host.check_output('curl -L  127.0.0.1/searchengine ')
     assert 'OMERO.web - Login' in out
