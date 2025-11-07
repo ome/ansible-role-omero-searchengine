461ac5a42efc7c44fbe253fd66375b5c01cb9d7c
khaledk2 <khaledk2@hotmail.com>
2025-11-07 22:01:31 +0000

    fix

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -28,5 +28,8 @@ def test_searchengine_elastic_cluster_health(host):
 #    assert  '"error"' in out
 
 def test_searchengine_search_elastic_connection(host):
-    out = host.check_output('curl -k -u  "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata')
+    out = host.check_output('curl -k -u  "elastic:elastic_password" https://127.0.0.1:9201/_component_template')
     assert '{"image_keyvalue_pair_metadata":{"aliases":{},"mappings":' in out
+
+
+#image_keyvalue_pair_metadata
