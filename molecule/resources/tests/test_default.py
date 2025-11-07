abd76188d77b25729d98b2949948b2c2e1532553
khaledk2 <khaledk2@hotmail.com>
2025-11-07 18:22:58 +0000

    Fix

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -23,7 +23,10 @@ def test_searchengine_elastic_cluster_health(host):
     assert  '"status" : "green",' in out
 
 
-def test_searchengine_search_elastic_connection(host):
-    out = host.check_output('http://127.0.0.1:8080/searchengine//api/v1/resources/image/searchvaluesusingkey/?key=cell%20line')
-    assert  '"error"' in out
+#def test_searchengine_search_elastic_connection(host):
+#    out = host.check_output('http://127.0.0.1:8080/searchengine//api/v1/resources/image/searchvaluesusingkey/?key=cell%20line')
+#    assert  '"error"' in out
 
+def test_searchengine_search_elastic_connection(host):
+    out = host.check_output('curl -k -u  "curl  -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata')
+    assert '{"image_keyvalue_pair_metadata":{"aliases":{},"mappings":' in out
