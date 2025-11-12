539174f46755c363e564e132b737a44bf935f085
khaledk2 <khaledk2@hotmail.com>
2025-11-12 11:57:26 +0000

    Fix

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -19,7 +19,7 @@ def test_searchengine_connection(host):
     assert 'OMERO search engine (API V1)' in out
 
 def test_searchengine_elastic_cluster_health(host):
-    out = host.check_output('curl  -k -u "elastic:elastic_password" https://127.0.0.1:9201/_cluster/health?pretty')
+    out = host.check_output('curl  -k -u "elastic:elastic_password" https://127.0.0.1:9200/_cluster/health?pretty')
     assert  '"status" : "green",' in out
 
 
@@ -28,7 +28,7 @@ def test_searchengine_elastic_cluster_health(host):
 #    assert  '"error"' in out
 
 def test_searchengine_search_elastic_connection(host):
-    out = host.check_output('curl -k -u  "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata')
+    out = host.check_output('curl -k -u  "elastic:elastic_password" https://127.0.0.1:9200/image_keyvalue_pair_metadata')
     assert '{"image_keyvalue_pair_metadata":{"aliases":{},"mappings":' in out
 
 
