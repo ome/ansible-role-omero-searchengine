6bac0196a67dd0a68ec59a8dfaacb7dfc58e8192
khaledk2 <khaledk2@hotmail.com>
2025-11-15 11:43:01 +0000

    Fix

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -26,6 +26,8 @@ def test_searchengine_connection(host):
 #    out = host.check_output('curl -XGET -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1 | | head -n1')
 #    assert '"image_keyvalue_pair_metadata_1":{"aliases":' in out
 
-def test_searchengine_elasticsearch_connection(host):
-    out = host.check_output('curl -s -o /dev/null -w "%{http_code}"   -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1')
+def test_create_search_index(host):
+    #out = host.check_output('curl -s -o /dev/null -w "%{http_code}"   -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1')
+    out = host.check_output(
+        'curl -I  -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1')
     assert '200' in out
