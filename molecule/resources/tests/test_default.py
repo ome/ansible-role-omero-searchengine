d47b3a0e64ad47c6181e199a7c65cee9e00c68db
khaledk2 <khaledk2@hotmail.com>
2025-11-15 07:57:03 +0000

    Fix

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -7,7 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_nginx_gateway(host):
-    out = host.check_output('curl -L  http://127.0.0.1:8080/searchengine/api/v1/resources/')
+    out = host.check_output('curl  http://127.0.0.1:8080/searchengine/api/v1/resources/')
     assert 'OMERO search engine (API V1)' in out
 
 def test_redis_connection(host):
@@ -15,15 +15,15 @@ def test_redis_connection(host):
     assert 'PONG' in out
 
 def test_searchengine_connection(host):
-    out = host.check_output('curl -L  http://127.0.0.1:5577/searchengine/api/v1/resources/')
+    out = host.check_output('curl  http://127.0.0.1:5577/searchengine/api/v1/resources/')
     assert 'OMERO search engine (API V1)' in out
 
 def test_searchengine_elastic_cluster_health(host):
-    out = host.check_output('curl  -L -k -u "elastic:elastic_password" https://127.0.0.1:9201/_cluster/health?pretty')
+    out = host.check_output('curl  -k -u "elastic:elastic_password" https://127.0.0.1:9201/_cluster/health?pretty')
     assert  '"status" : "green",' in out
 
 
-#def test_searchengine_elasticsearch_connection(host):
-#    out = host.check_output('curl  -L -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1')
-#    assert '"image_keyvalue_pair_metadata":{"aliases":{},"mappings":' in out
+def test_searchengine_elasticsearch_connection(host):
+    out = host.check_output('curl  -k -u "elastic:elastic_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1')
+    assert '"image_keyvalue_pair_metadata":{"aliases":{},"mappings":' in out
 
