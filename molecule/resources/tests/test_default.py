c343dda8cb0c8858b9ee9b5588d22b7f1748d4e9
khaledk2 <khaledk2@hotmail.com>
2025-11-23 17:22:29 +0000

    test

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -11,13 +11,13 @@ def test_create_search_index(host):
         'curl -I  -k -u "elastic:elastic_password" https://127.0.0.1:9201/')
     assert '200' in out
 
-def test_nginx_gateway(host):
-    out = host.check_output('curl -XGET http://127.0.0.1:8080/searchengine/api/v1/resources/')
-    assert 'OMERO search engine (API V1)' in out
+#def test_nginx_gateway(host):
+#    out = host.check_output('curl -XGET http://127.0.0.1:8080/searchengine/api/v1/resources/')
+#    assert 'OMERO search engine (API V1)' in out
 
-def test_redis_connection(host):
-    out = host.check_output('redis-cli ping')
-    assert 'PONG' in out
+#def test_redis_connection(host):
+#    out = host.check_output('redis-cli ping')
+#    assert 'PONG' in out
 
 def test_searchengine_connection(host):
     out = host.check_output('curl -XGET http://127.0.0.1:5577/searchengine/api/v1/resources/')
